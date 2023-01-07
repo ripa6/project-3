fb7b9a2865e456a11d63e8caa1855901cabeed66
ripa6 <121214730+ripa6@users.noreply.github.com>
2023-01-07 22:10:17 +0600

    Add files via upload

---
+++ js/style.js
@@ -0,0 +1,27 @@
+
+(function($){
+	'use strict';
+
+//code start
+
+
+$('button.item-4').on('click', function(){
+	$('.section-2'). fadeIn ();
+});
+
+
+$('.item-3 i').on ('click',function(){
+	$('.section-2'). fadeOut ();
+});
+
+
+
+
+new VenoBox();
+
+
+
+
+
+
+}) (jQuery);
